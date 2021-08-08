823f37636b181ccbc2d819d132db5b65588d94ab
bardonfro <59987699+bardonfro@users.noreply.github.com>
2021-08-07 23:10:31 -0600

    Tab styling and start page

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ let pages =[
     {name: "Menu", render: menu.render},
     {name: "Contact", render: contact.render},
 ];
+const startPage = pages[0];
 
 const pageWrapper = dmy.el("div#page-wrapper");
     document.body.appendChild(pageWrapper);
@@ -39,14 +40,25 @@ const contentWrapper = dmy.el("div#content");
 
 function newNavTab(obj) {
     const btn = dmy.el("div.nav-tab",obj.name);
+    obj.button = btn;
     btn.onclick = clickNavTab.bind(obj);
+
     return btn;
 }
 
 function clickNavTab() {
+    document.querySelectorAll('.active').forEach(function(element) {
+        element.classList.remove('active');
+    })
+
     contentWrapper.textContent = "";
     contentWrapper.appendChild(this.render());
+    this.button.classList.add("active");
 }
 
+
 //Initial page load - default to Home
-contentWrapper.appendChild(home.render());
+const init = (function() {
+    contentWrapper.appendChild(startPage.render());
+    startPage.button.classList.add("active");
+})();
